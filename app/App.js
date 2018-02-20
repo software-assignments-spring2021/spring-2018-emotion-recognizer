ab453d0162f3433b7fdc6f5ceef2acd10a3af3ea
Nathan Baum <nbaum510@gmail.com>
2018-02-20 17:32:19 -0500

    fixed package.json and .gitignore problems, and integrated firebase

--- app/App.js
+++ app/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import * as firebase from 'firebase';
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB2EvVasHywMAgEO30nOUBerazLu61yMc8 ",
+  authDomain: "moodio-f230e.firebaseapp.com",
+  databaseURL: "https://moodio-f230e.firebaseio.com/",
+  storageBucket: "moodio-f230e.appspot.com"
+};
+
+/*
+A Firebase App can use multiple Firebase services. Each service can be accessed from the firebase namespace:
+
+    firebase.auth() - Authentication (https://firebase.google.com/docs/auth/?authuser=0)
+    firebase.storage() - Cloud Storage (https://firebase.google.com/docs/storage/?authuser=0)
+    firebase.database() - Realtime Database (https://firebase.google.com/docs/database/?authuser=0)
+    firebase.firestore() - Cloud Firestore (https://firebase.google.com/docs/firestore/?authuser=0)
+
+*/
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 export default class App extends React.Component {
   render() {
     return (
