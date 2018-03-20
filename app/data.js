a766b5d0b0ad29ecf8a808c24e160c954ff93a2d
Olivia Giddens <oliviagiddens@Olivias-MacBook-Air-3.local>
2018-03-20 00:05:41 -0400

    add a simple game in own branch

---
+++ app/data.js
@@ -0,0 +1,118 @@
+var data = [
+  {
+    "category": "Happy",
+    "questions": [
+      {
+        "points": 100,
+        //add pictures
+        "question": "<p>Which person is happy?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "B"
+      },
+      {
+        "points": 200,
+        //add pictures
+        "question": "<p>Which person is happy?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      },
+      {
+        "points": 300,
+        //add pictures
+        "question": "<p>Which person is happy?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "D"
+      },
+      {
+        "points": 400,
+        //add pictures
+        "question": "<p>Which person is happy?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "C"
+      }
+    ]
+  },
+  {
+    "category": "Upset",
+    "questions": [
+      {
+        "points": 100,
+        //add pictures
+        "question": "Which person is upset?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      },
+      {
+        "points": 200,
+        //add pictures
+        "question": "Which person is upset?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "D"
+      },
+      {
+        "points": 300,
+        //add pictures
+        "question": "Which person is upset?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      },
+      {
+        "points": 400,
+        //add pictures
+        "question": "Which person is upset?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "B"
+      }
+    ]
+  },
+  {
+    "category": "Excited",
+    "questions": [
+      {
+        "points": 100,
+        //add pictures
+        "question": "<p>Which person is excited?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "D"
+      },
+      {
+        "points": 200,
+        //add pictures
+        "question": "<p>Which person is excited?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "C"
+      },
+      {
+        "points": 300,
+        //add pictures
+        "question": "<p>Which person is excited?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "D"
+      },
+      {
+        "points": 400,
+        //add pictures
+        "question": "<p>Which person is excited?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      }
+    ]
+  },
+  {
+    "category": "Neutral",
+    "questions": [
+      {
+        "points": 100,
+        //add pictures
+        "question": "<p>Which person is neutral?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "C"
+      },
+      {
+        "points": 200,
+        //add pictures
+        "question": "<p>Which person is neutral?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      },
+      {
+        "points": 300,
+        //add pictures
+        "question": "<p>Which person is neutral?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "A"
+      },
+      {
+        "points": 400,
+        //add pictures
+        "question": "<p>Which person is neutral?</p><ol><li>A</li><li>B</li><li>C</li><li>D</li></ol>",
+        "answer": "C"
+      }
+    ]
+  }
+];
