ab610d7874f12ccf587788929dffea96a5bb8290
yc2311 <yc2311@github.com>
2018-04-30 16:31:08 -0400

    emoji app

---
+++ app/iosApps/build/Pods.build/Debug-iphoneos/Pods-AffdexEmoji.build/DerivedSources/Pods_AffdexEmoji_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_AffdexEmojiVersionString[];
+ extern const double Pods_AffdexEmojiVersionNumber;
+
+ const unsigned char Pods_AffdexEmojiVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_AffdexEmoji  PROJECT:Pods-1" "\n";
+ const double Pods_AffdexEmojiVersionNumber __attribute__ ((used)) = (double)1.;
